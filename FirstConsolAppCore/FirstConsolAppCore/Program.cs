6185c62351b48849068e8aa9f41f3f9877ac97ef
Dawid <dawidbuclaw@gmail.com>
2018-09-18 05:49:25 +0200

    rebase

--- FirstConsolAppCore/FirstConsolAppCore/Program.cs
+++ FirstConsolAppCore/FirstConsolAppCore/Program.cs
@@ -9,7 +9,7 @@ namespace FirstConsolAppCore
             Console.WriteLine("Hello World!");
         }
 
-        private int Addition(int a, int b)
+        private int Add(int aa, int bb)
         {
             return a + b;
         }
